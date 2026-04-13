8e115b1c28b2500d401308b87db5bd4a56e18a8a
Mahak Gupta <2k24.cs1j.2411302@gmail.com>
2026-04-13 23:06:08 +0530

    Initial commit: Node.js backend with ES modules, math utilities, fruits module, and figlet setup

---
+++ Fruits/index.js
@@ -0,0 +1,6 @@
+// index.js file baaki saari files ka data acquire krti and fir ise export krti hai.
+const apple=require("./apple");
+const banana=require("./banana");
+const orange=require("./orange");
+let fruits=["apple","banana","orange"];
+module.exports=fruits;
